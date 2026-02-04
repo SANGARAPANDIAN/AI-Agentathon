c9c455f3aaa11bff85e3223040da0df8c3127902
Nithish.S <nithish.s.1622@gmail.com>
2026-02-04 15:24:19 +0530

    timer4

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -10,7 +10,7 @@ interface TimeLeft {
 }
 
 const Countdown = () => {
-  const targetDate = new Date(2026, 1, 4, 15, 30, 0, 0).getTime();
+  const targetDate = new Date(2026, 1, 4, 16, 00, 0, 0).getTime();
 
   const calculateTimeLeft = (): TimeLeft => {
     const now = new Date().getTime();
